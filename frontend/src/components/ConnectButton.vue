9fd820390a38a0b8d60d8cb320ea5fb38838129d
Cyril Florval <ascread@gmail.com>
2022-02-18 01:57:04 +0100

    View Connect

---
+++ frontend/src/components/ConnectButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="button">
+        <button class="ChangeTemplate" @click="emitChangeTemplate"> {{ button_name }} </button>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "ConnectButton",
+    components: {
+    
+    },
+
+    data() {
+        return {
+            
+        }
+    },
+
+    methods: {
+        emitChangeTemplate() {
+            this.$emit('change-template')
+        }
+    },
+
+    computed: {
+        
+    },
+}
+</script>
+
+
+<style lang="scss">
+    
+</style>
---
+++ frontend/src/views/Connect.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="Connect">
+        <Header />
+
+        <div class="form">
+
+        </div>
+        
+    </div>
+</template>
+
+
+<script>
+import Header from "@/components/Header.vue"
+
+
+export default {
+    name: "Connect",
+    components: {
+        Header
+    }
+
+}
+</script>
+
+
+<style lang="scss">
+    
+</style>
